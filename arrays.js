c031e8a9789b86816c0017a066964193a9d3c8ee
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-09-12 20:29:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,3 +44,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array) {
+  var array
+  array.slice(0, array.length - 1)
+  return array
+}
